e6679380f305336ad60313121d2c7bbfef0f9f37
prey95 <patrick.reyes2@g.austincc.edu>
2023-06-17 23:27:16 -0500

    homework2

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,17 @@
 //write include statements
+#include "decisions.h"
 
 int main() 
 {
+	int grade;
+	char Lgrade;
+
+	cout << "Hello, please enter your grade: ";
+	cin >> grade;
+
+	Lgrade = get_letter_grade_using_switch(grade);
+
+	cout << "The letter grade for " << grade << " is: " << Lgrade << "\n";
+
 	return 0;
 }

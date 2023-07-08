e705c95cc8b23b8d331d6b5a4f74ea8b323e235e
prey95 <patrick.reyes2@g.austincc.edu>
2023-07-07 20:51:24 -0500

    homework 4

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,34 @@
+#include "tic_tac_toe.h"
+
+using std::cin;
 
 int main() 
 {
+	TicTacToe game;
+	string first_player;
+	char user_choice = 'y';
+
+	do
+	{
+		cout<<"Enter first player: ";
+		cin>>first_player;
+
+		game.start_game(first_player);
+
+		int position;
+
+		while(!game.game_over())
+		{
+			cout<<"Enter a position: ";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		cout<<"Play again? y or Y?";
+		cin>>user_choice;
+
+	}while(user_choice == 'y' || user_choice == 'Y');
 
 	return 0;
 }
--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,30 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test if board is full") {
+	TicTacToe game;
+
+	game.mark_board(1);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(2);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(3);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(4);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(5);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(7);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(6);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(9);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(8);
+	REQUIRE(game.game_over() == true);
+}

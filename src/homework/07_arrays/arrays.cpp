174629eca2042b50e518b98246d63f0cd0dcfee1
prey95 <patrick.reyes2@g.austincc.edu>
2023-07-21 09:57:18 -0500

    homework 8

--- src/homework/07_arrays/arrays.cpp
+++ src/homework/07_arrays/arrays.cpp
@@ -1,1 +1,70 @@
-//add function(s) code here
+#include "arrays.h"
+
+ArrayList::ArrayList(int* array_list, int capacity) :
+    elements(array_list)
+{
+    this->capacity = capacity;
+}
+
+void ArrayList::Add(int value)
+{
+    if(current_index < capacity)
+    {
+        elements[current_index] = value;
+        current_index++;
+    }
+}
+
+int ArrayList::Get_First_Index_Of(int value)
+{
+    auto index = -1;
+    auto local_current_index = 0;
+    
+    while(local_current_index < current_index)
+    {
+        if(elements[local_current_index] == value)
+        {
+            index = local_current_index;
+            break;
+        }
+
+        local_current_index++;
+    }
+
+    return index;
+}
+
+int ArrayList::Get_Value_At_Index(int index)
+{
+    int value = -1;
+
+    if(index >= 0 && index < current_index)
+    {
+        value = elements[index];
+    }
+
+    return value;
+}
+
+void ArrayList::Delete(int value)
+{
+    auto index = Get_First_Index_Of(value);
+
+    if(index != -1)
+    {
+        AdjustElements(index);
+    }
+}
+
+void ArrayList::AdjustElements(int index)
+{
+    //class assignment to complete
+
+    while(index < current_index-1)
+    {
+        elements[index] = elements[index+1];
+        index++;   
+    }
+    current_index--;
+
+}
